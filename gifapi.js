21730419614de79ba70e42dde90d9d2aded93f50
Edward <edwardgretsch@gmail.com>
2018-06-11 04:06:30 -0700

    update3

--- gifapi.js
+++ gifapi.js
@@ -1,25 +1,44 @@
 var butions = [];
 
-// function displayMovieInfo() {
 
-//     var movie = $(this).attr("data-name");
-//     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+function topic() {
+var search = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         $("#movies-view").text(JSON.stringify(response));
-//         renderButtons();
-//     });
-// }
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+        console.log(queryURL);
+        
+    var results = response.data;
+    for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='search'>");
+        var p = $("<p>").text("Rating: " + results[i].rating);
+        var gifImage = $("<img>");
+        gifImage.attr("data-state");
+        gifImage.attr(
+            {
+                "data-state": "still",
+                "data-animate": results[i].images.fixed_height.url,
+                "data-still": results[i].images.fixed_height_still.url
+            }
+        )
+        console.log(results[i].images.fixed_height_still.url);
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+        $("#gifs").prepend(gifDiv);
+    }
+
+    });
+}
 
 
 function renderButtons() {
     $("#buttons").empty();
     for (var i = 0; i < butions.length; i++) {
         var a = $("<button>");
-        a.addClass("button");
+        a.addClass("gifty");
         a.attr("data-name", butions[i]);
         a.text(butions[i]);
         $("#buttons").append(a);
@@ -28,13 +47,14 @@ function renderButtons() {
 
 $("#add-button").on("click", function (event) {
     event.preventDefault();
+    $("#button-input").empty();
     var button2 = $("#button-input").val().trim();
     butions.push(button2);
     renderButtons();
-
+    
 });
 
-$(document).on("click", ".buttion", alertMovieName);
+$(document).on("click", ".gifty", topic);
 
 
 renderButtons();
